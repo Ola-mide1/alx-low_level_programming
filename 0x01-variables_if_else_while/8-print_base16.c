72b00f2751c7c4fcc30be87e7906c80d629e28de
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-09 09:40:27 -0800

    task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return number of base 16 in lowercase
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char c = 'a';
+	int num;
+
+	for (num = 0; num < 16; num++)
+	{
+		if (num < 10)
+		{
+			putchar(num + '0');
+		}else
+		{
+			putchar(c);
+			c++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+		
+	
+			
+	
