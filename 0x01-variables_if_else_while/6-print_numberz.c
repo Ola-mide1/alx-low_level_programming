a08f69fed1d29d83278fb0ee7bec9051a4519e7a
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-09 09:10:06 -0800

    task 7

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - return digit number of base 10 starting from 0
+ * main - return digit base 10
  *
  * Return: Always 0 (Success)
  *
@@ -11,7 +12,7 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 0; num++)
+	for (num = 0; num < 10; num++)
 		putchar(num + '0');
 	putchar('\n');
 	return (0);
