e8f22bf5a7575d9ec0b718e27dddba4f18b37836
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-09 10:59:15 -0800

    task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -25,7 +25,8 @@ int main(void)
 					{
 						putchar(i);
 						putchar(n);
-						putchar(j);						if (i == 55 && n == 56 && j == 57)
+						putchar(j);
+						if (i == 55 && n == 56 && j == 57)
 						{
 							break;
 						}
