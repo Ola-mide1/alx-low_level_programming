5227d1534f75fec8b6cf804f14dfb8fced7ad652
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-09 09:17:21 -0800

    task 8

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - return lower case alphabet in reverse
+ *
+ * Returns: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
