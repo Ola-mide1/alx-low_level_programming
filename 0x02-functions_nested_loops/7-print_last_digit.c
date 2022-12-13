fe0ca79ecb7264673637deecb42c6c69ac967fd6
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-13 02:52:21 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> prints last digit of a number
+ * @n: the passed args
+ * Return: the last digit
+ */
+
+int print_last_digit(int n)
+{
+	int x;
+
+	if n < 0
+		n = -n;
+	x = n % 10;
+	_putchar(X + '0');
+	return (x);
+}
