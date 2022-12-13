1650c6ee709bebe47e3168895f430efa9189d90d
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-13 00:46:02 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry Point
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (char <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+
+	_putchar('\n');
+}
