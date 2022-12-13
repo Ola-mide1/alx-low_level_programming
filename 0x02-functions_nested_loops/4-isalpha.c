cdde0546950b8fb194e8062411063aae09ac4d95
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-13 04:20:31 -0800

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,8 @@
 
 /**
  * _isalpha -> checks for alphabetical letters
- *
- * @c: a character to be checked on
- *
- * Return: returs 0 or 1 depending on condition
- *
+ * @c: a charter to be checked on
+ * Return: returns 0 or 1 depending on condition
  */
 
 int _isalpha(int c)
