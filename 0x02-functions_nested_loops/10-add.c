ac6ccf4bb0330e7ec59c1d212f010c3f9a5ca541
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-13 05:30:32 -0800

    task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * * add -> adds two number
- * * @x : param1
- * * @y : param2
- * * Return: x + y
- * */
+ * add -> adds two number
+ * @x : param1
+ * @y : param2
+ * Return: x + y
+ */
 
 int add(int x, int y)
 {
