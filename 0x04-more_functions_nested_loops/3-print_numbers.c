1b02ee3c2d6e0124a3d9e78f951d588bffe55d0f
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-15 12:36:57 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers
+ * print_numbers -> print numbers
  */
 
 void print_numbers(void)
@@ -9,6 +9,6 @@ void print_numbers(void)
 	int x;
 
 	for (x = 0, x < 10; x++)
-		_putchar(n + '0');
+		_putchar(x + '0');
 	_putchar('\n');
 }
