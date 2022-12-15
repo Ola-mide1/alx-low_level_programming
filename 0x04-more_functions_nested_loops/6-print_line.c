ec77d2cf0c39f42f9041ac77d88f319f8a5587f8
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-15 12:24:00 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a straight line using a character _
+ * @n: the number of _ character printed
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
