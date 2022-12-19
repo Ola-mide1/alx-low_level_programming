4e9221b69cf6fbba28d651f654579c238b9b4e3a
ola-mide1 <olamidesuccess9@gmail.com>
2022-12-19 15:46:00 -0800

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,21 +7,18 @@
 
 void rev_string(char *s)
 {
-	char tmp;
-	int i, length1, length2;
+	int c, revs;
+	char character;
 
-	length1 = 0;
-	length2 = 0;
-
-	while (s[length1] != '\0')
-		length1++;
-
-	length2 = length1 - 1;
-	for (i = 0; i < length1 / 2; i++)
-		{
-			tmp = s[i];
-			s[i] = s[length2];
-			s[length2] = tmp;
-			length2 -= 1;
-		}
+	for (c = '\0\; s[c] !=0; c++)
+	{
+	}
+	revs = 0;
+	for (c = c - 1; revs < c; revs++)
+	{
+		character = s[c];
+		s[c] = s[revs];
+		s[revs] = character;
+		c -;
+	}
 }
