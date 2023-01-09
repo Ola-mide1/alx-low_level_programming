ba08f5aead72fea5ce3096af59b7e86e6c021f45
Ola-mide1 <olamidesuccess9@gmail.com>
2023-01-09 00:47:02 -0800

    task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -30,7 +30,7 @@ char *create_array(unsigned int size, char c)
 		while (position < size) /*While for array*/
 		{
 			*(buffer + position) = c;
-			position++
+			position++;
 		}
 
 		return (buffer);
