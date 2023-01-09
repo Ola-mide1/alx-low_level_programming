8060159b29b7710f333a72c0a5f36407b5a0a682
Ola-mide1 <olamidesuccess9@gmail.com>
2023-01-09 01:04:29 -0800

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,69 +1,27 @@
 #include "main.h"
-/**
- * _strlen - count array
- * @s: array of elements
- * Return: 1
- */
-
-int _strlen(char *s)
-{
-	unsigned int i;
-
-	i = 0;
-	while (s[i] != '\0') /*count character of string*/
-	{
-		i++;
-	}
-
-	return (1);
-}
 
 /**
- * _strcpy - copy arrays
- * @src: array of elements
- * @dest: dest array
- * Return: dest
+ * _strdup -> string duplicator function
+ * @str: string to be dupliated
+ *Return: a string pointer
  */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-
-	return (dest);
-}
-
-/**
- * _strdup - array for prints a string
- * @str: array of elements
- * Return: pointer
- */
-
 char *_strdup(char *str)
 {
-	char *dst;
-	unsigned int size;
+	int i = 1, j = 0;
+	char *s;
 
-	if (str == 0)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
-
-	size = _strlen(str) + 1;
-
-	dst = (char *) malloc(size * sizeof(char));
-
-	if (dst == 0)
-	{
+	while (str[i])
+		i++;
+	s = (char *)malloc(i * sizeof(char) + 1);
+	if (s == NULL)
 		return (NULL);
+	while (j < i)
+	{
+		s[j] = str[j];
+		j++;
 	}
-
-	_strcpy(dst, str);
-	return (dst);
+	s[j] = '\0';
+	return (s);
 }
