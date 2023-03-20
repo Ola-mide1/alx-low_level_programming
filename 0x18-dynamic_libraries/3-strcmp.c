f48b9a239ab7cf5a3cf68031635c9a774c5895c9
Ola-mide1 <olamidesucces9@gmail.com>
2023-03-20 06:22:06 -0700

    fixed

---
+++ 0x18-dynamic_libraries/3-strcmp.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp -> for comparing purpose
+ * @s1: first character
+ * @s2: 2nd character
+ * Return: integer
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+
+	result = strcmp(s1, s2);
+	return (result);
+}
